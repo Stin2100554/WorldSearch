688acaa2a4c0892b3c5a5d7e46422e8c048c0063
Sam Tindale <2100554@csl-student.wlv.ac.uk>
2022-04-01 09:13:24 +0100

    Add Dark and Light mode hardware API functionality

--- app/Views/formatting/header.php
+++ app/Views/formatting/header.php
@@ -2,8 +2,31 @@
 <html>
 	<head>
 		<title>WorldSearch</title>
-		<link rel="stylesheet" href="<?=base_url('styles/MainStyle.css')?>" type="text/css">
-		<link rel="icon" href="<?=base_url('images/favicon.ico')?>" type="image/x-icon">
+		<link id="lightMode" rel="stylesheet" href="<?=base_url('styles/MainStyle.css')?>" type="text/css" />
+		<link id="darkMode" rel="stylesheet" href="<?=base_url('styles/DarkStyle.css')?>" type="text/css" />
+		<link rel="icon" href="<?=base_url('images/favicon.ico')?>" type="image/x-icon" />
+		
+		<script>
+		//Detects if the user uses dark mode on their device.
+		if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
+			console.log('Styleswitcher supported!');
+		} else {
+			console.log('Incompatible with styleswitcher!');
+		}
+		//0 = Light
+		//1 = Dark
+		const usingDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+		usingDarkMode.addListener(x => {
+			if (x.matches) {
+				//If the visitor uses dark mode, run this script.
+				document.styleSheets[1].disabled = false;
+			} else {
+				//If the user uses light mode or the script fails for whatever reason, run this.
+				document.styleSheets[1].disabled = true;
+			}
+			//Ideally, this can react to changes to the style, so the alternate style should be disabled.
+		});
+		</script>
 	</head>
 	
 	<body>
